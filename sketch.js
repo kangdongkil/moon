395757fa9bea3a94b69e7169093c8e61f565e5fa
kangdongkil <kbk3418@naver.com>
2014-11-06 16:01:21 +0900

    a

--- sketch.js
+++ sketch.js
@@ -24,8 +24,8 @@ function draw(){
         50);                                 // height
   
   fill(255,0,255,100);
-  image(b,240+Math.cos(timing*2*PI)*150,                     // x좌표
-        240 + Math.sin(timing*2*PI)*150 , // y좌표
+  image(b,200+Math.cos(timing*2*PI)*150,                     // x좌표
+        200 + Math.sin(timing*2*PI)*150 , // y좌표
         50,                                  // width
         50);
   noFill();
